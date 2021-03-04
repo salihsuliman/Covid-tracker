aff595cc07093c82a0d0379bdf1d1fe2397265bb
salihsuliman <gusicinc@gmail.com>
2021-03-04 18:52:53 +0000

    comit

---
+++ build/precache-manifest.2b8fc2c72ed69bfc5d3a7bc1d7f81d81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c94841bcfb3657b16d3488c6b6011c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f1bc366c884e3860956",
+    "url": "/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "2e9351cdcec7e06560bb",
+    "url": "/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "3f1bc366c884e3860956",
+    "url": "/static/js/2.637ba975.chunk.js"
+  },
+  {
+    "revision": "b29fa9d568ccdfac989d0d2ab5fd3892",
+    "url": "/static/js/2.637ba975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e9351cdcec7e06560bb",
+    "url": "/static/js/main.4a461a92.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
